b1f648fbca7bad2b5fd94258ddd36e6e63d8ccd1
Akriti Mishra <73214701+akriti6101@users.noreply.github.com>
2023-04-12 11:40:49 +0530

    added welcome page

---
+++ welcome.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+data=[{"firstname":"Akriti " , "lastname ":"Mishra" ,"company":"Abzooba"},
+       {"firstname":"Sunny " , "lastname ":"Singh" ,"company":"Cognizant"},
+       {"firstname":"Priya " , "lastname ":"Mehra" ,"company":"Tcs"},
+      ]
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+   return data
+
+if __name__ =='__main__':
+   app.run(debug=True)
+   print('oroo')
