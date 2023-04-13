d3154b69b34e20d4e177c000d38f4894b7aa7022
Akriti Mishra <73214701+akriti6101@users.noreply.github.com>
2023-04-13 10:27:44 +0530

    commit

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app=Flask(__name__)
 @app.route("/")
 def home():
    return data
+@app.route("/home")
+def home():
+   return render_template('home.html')
 
 if __name__ =='__main__':
    app.run(debug=True)
